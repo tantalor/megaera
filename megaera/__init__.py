1cb67092a8bd26a92d94e45e23dee3794696d38d
John Tantalo <john.tantalo@gmail.com>
2011-02-19 15:57:50 -0800

    use jinja2 symlink

--- megaera/__init__.py
+++ megaera/__init__.py
@@ -1,11 +1,2 @@
-import sys
-import os
-
-# add jinja to module path
-sys.path.insert(0, os.sep.join([
-  os.path.abspath(os.path.dirname(__file__)),
-  '..', 'vendor', 'jinja2'
-]))
-
 from request_handler import RequestHandler, NotFoundException
 from to_xml import to_xml
