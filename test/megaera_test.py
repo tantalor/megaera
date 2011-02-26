c2b624f93742070768e9ebde3c4b81459b75c01a
John Tantalo <john.tantalo@gmail.com>
2011-02-25 20:17:07 -0800

    updated tests for jinja templates

--- test/megaera_test.py
+++ test/megaera_test.py
@@ -8,12 +8,15 @@ from google.appengine.ext.webapp import Request, Response
 from google.appengine.api import apiproxy_stub_map
 from google.appengine.api.memcache import memcache_stub
 
+class MockJinja:
+  def get_template(self, path):
+    self.path = path
+    self.template = MockTemplate()
+    return self.template
 
 class MockTemplate:
-  def render(self, path, vars):
-    self.path = path
-    self.vars = vars
-
+  def render(self, **kwargs):
+    self.kwargs = kwargs
 
 def mock_page(file=''):
   page = types.ModuleType('MockPage')
@@ -24,6 +27,7 @@ def mock_handler(file='handlers/mock.py', request='/mock', **response):
   handler = RequestHandler.with_page(mock_page(file))()
   handler.initialize(Request.blank(request), Response())
   handler.response_dict(**response)
+  handler.jinja = MockJinja()
   return handler
 
 
@@ -36,7 +40,6 @@ def stub_memcache():
 
 class TestMegaera(unittest.TestCase):
   def setUp(self):
-    self.mock_template = megaera.request_handler.template = MockTemplate()
     stub_memcache()
   
   def test_with_page(self):
@@ -80,14 +83,14 @@ class TestMegaera(unittest.TestCase):
     handler = mock_handler(file='handlers/foo/bar.py')
     handler.file_exists = lambda self: True
     handler.render(None)
-    self.assertEquals(self.mock_template.path, 'templates/foo/bar.html')
+    self.assertEquals(handler.jinja.path, 'foo/bar.html')
   
   def test_atom_render(self):
     handler = mock_handler(file='handlers/foo/bar.py', request='/?atom')
     handler.file_exists = lambda self: True
-    handler.render(None)
+    handler.render(None, 'atom')
     self.assertTrue(handler.is_atom())
-    self.assertEquals(self.mock_template.path, 'templates/foo/bar.html')
+    self.assertEquals(handler.jinja.path, 'foo/bar.atom')
   
   def test_cache(self):
     handler = mock_handler()
