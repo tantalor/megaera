174e473d1ee7c203dcec59fdc7489db80382017a
John Tantalo <john.tantalo@gmail.com>
2011-08-19 22:55:36 -0700

    setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='Megaera',
+  description='Megaera Request Handler for Google App Engine',
+  author='John Tantalo',
+  author_email='john.tantalo@gmail.com',
+  url='http://github.com/tantalor/megaera',
+  packages=['megaera'],
+)
