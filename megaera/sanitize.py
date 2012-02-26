be0cd45f157c274142bc0fab90e331b46008c5c1
John Tantalo <john.tantalo@gmail.com>
2012-02-25 21:32:51 -0800

    added urlize lambda

--- megaera/sanitize.py
+++ megaera/sanitize.py
@@ -1,16 +1,16 @@
 from google.appengine.api import datastore_types
 
 
-def sanitize(obj):
+def sanitize(obj, urlize):
   """Sanitize for json or yaml output."""
   if isinstance(obj, dict):
     # a dictionary
-    return dict([(key, sanitize(value)) for key, value in obj.iteritems()])
+    return dict([(key, sanitize(value, urlize)) for key, value in obj.iteritems()])
   if hasattr(obj, '__iter__'):
     # a iterable sequence
-    return [sanitize(v) for v in obj]
+    return [sanitize(v, urlize) for v in obj]
   if hasattr(obj, 'sanitize'):
     # a sanitizeable object
-    return obj.sanitize()
+    return obj.sanitize(urlize)
   # default: stringify
   return str(obj)
